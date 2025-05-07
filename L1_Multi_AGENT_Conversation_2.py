60d4e8bb92e23caa6846fa8866b4b14769ee1d8c
Mohammad Bagher Ehtemam <ehtemam@gmail.com>
2025-05-07 20:58:39 +0200

    add custom termination message

--- L1_Multi_AGENT_Conversation_2.py
+++ L1_Multi_AGENT_Conversation_2.py
@@ -8,25 +8,33 @@ llm_config = { "model": "gpt-3.5-turbo"}
 cathy = ConversableAgent(
     name="cathy",
     system_message="Your name is Cathy and"
-    "and you are a stand-up comedian.",
+    "and you are a stand-up comedian."
+    "When you ready to end the conversation say 'I gotta go",
     llm_config=llm_config,
-    human_input_mode="NEVER"
+    human_input_mode="NEVER",
+    is_termination_msg=lambda msg: "I gotta go" in msg["content"]
 )
 
 joe = ConversableAgent(
     name="joe",
     system_message=
     "You name is Joe and you are a stand-up comedian."
-    "Start the next joke from the punchline of the previous joke.",
+    "Start the next joke from the punchline of the previous joke."
+    "When you ready to end the conversation say 'I gotta go",
     llm_config=llm_config,
-    human_input_mode="NEVER"
+    human_input_mode="NEVER",
+    is_termination_msg=lambda msg: "I gotta go" in msg["content"]
 )
 
 chat_result = joe.initiate_chat(
     recipient=cathy,
     message="I'm Joe.Cathy, let's keep the jokes rolling",
-    max_turns=2
+    max_turns=2,
+    summary_method="reflection_with_llm",
+    summary_prompt="Summarize the conversation"
 )
+
+cathy.send(message="What's last joke we talked about", recipient=joe)
 
 pprint(chat_result.chat_history)
 pprint(chat_result.cost)
