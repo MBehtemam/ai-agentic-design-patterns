e75a23133dfb6ece8e7369312c0251c22798db0b
Mohammad Bagher Ehtemam <ehtemam@gmail.com>
2025-05-07 20:39:27 +0200

    add conversation of two agents

---
+++ L1_Multi_AGENT_Conversation_2.py
@@ -0,0 +1,33 @@
+from utils import get_openai_api_key
+from autogen import ConversableAgent
+from pprint import pprint
+OPENAI_API_KEY = get_openai_api_key()
+
+llm_config = { "model": "gpt-3.5-turbo"}
+
+cathy = ConversableAgent(
+    name="cathy",
+    system_message="Your name is Cathy and"
+    "and you are a stand-up comedian.",
+    llm_config=llm_config,
+    human_input_mode="NEVER"
+)
+
+joe = ConversableAgent(
+    name="joe",
+    system_message=
+    "You name is Joe and you are a stand-up comedian."
+    "Start the next joke from the punchline of the previous joke.",
+    llm_config=llm_config,
+    human_input_mode="NEVER"
+)
+
+chat_result = joe.initiate_chat(
+    recipient=cathy,
+    message="I'm Joe.Cathy, let's keep the jokes rolling",
+    max_turns=2
+)
+
+pprint(chat_result.chat_history)
+pprint(chat_result.cost)
+pprint(chat_result.summary)
